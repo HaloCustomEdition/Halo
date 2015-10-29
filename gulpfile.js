0ed4eca0935151619aceeb9b6e039603fc02562f
Janik von Rotz <login@janikvonrotz.ch>
2015-10-29 16:31:30 +0100

    update readme layout

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 var gulp = require('gulp');
 var markdown = require('gulp-markdown');
 var rename = require('gulp-rename');
+var wrapper = require('gulp-wrapper');
+var cheerio = require('gulp-cheerio');
 
 gulp.task('default', function () {
     return gulp.src('README.md')
         .pipe(markdown())
         .pipe(rename('index.html'))
+        .pipe(wrapper({
+           header: '<html><head></head><body><div class="container">\n',
+           footer: '</div></body></html>\n'
+        }))
+        .pipe(cheerio(function ($, file) {
+          $("head").append("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'  type='text/css'>");
+        }))
         .pipe(gulp.dest('out'));
 });
